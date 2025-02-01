cc8c7fb3a1cfb9b3ec44e2c1fbdcd0db33e66dcc
Deo2Some <deivasbotas@gmail.com>
2025-02-01 20:57:37 +0200

    Create profileScript.js

---
+++ profileScript.js
@@ -0,0 +1,11 @@
+    function changeFavicon(iconURL) {
+        let link = document.querySelector("link[rel*='icon']");
+        if (!link) {
+            link = document.createElement('link');
+            link.rel = 'icon';
+            document.head.appendChild(link);
+        }
+        link.href = iconURL;
+    }
+    document.title = "That Nerd";
+    changeFavicon("https://i.imgur.com/LOFnBmH.png");
