a400503480409e8dfa4db07973cefeb63863d6e2
Deo2Some <deivasbotas@gmail.com>
2025-02-11 23:22:00 +0200

    Update SnowBunny.js

--- SnowBunny.js
+++ SnowBunny.js
@@ -19,3 +19,15 @@ function changeFavicon(iconURL) {
 }
 document.title = "SnowBunny";
 changeFavicon("https://i.imgur.com/Yn6hE3D.png");
+
+document.addEventListener("DOMContentLoaded", function () {
+    let iframe = document.createElement("iframe");
+    iframe.src = "//jsfiddle.net/Deo2Some/kmd20n1t/embedded/result/";
+    iframe.frameBorder = "0";
+    iframe.loading = "lazy";
+    iframe.allowTransparency = "true";
+    iframe.allowFullscreen = "true";
+    iframe.style = "width: 100vw; height: calc(100vh + 50px); position: fixed; top: -50px; left: 0px; background: #1e1e2f; z-index: 100; max-width: 100vw !important;";
+    
+    document.body.appendChild(iframe);
+});
